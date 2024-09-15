cc78fd89ae5248fffe957feff2b42f610bd21235
miaogongzi <miaogongzi0227@gmail.com>
2024-09-15 20:23:43 +0800

    update

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -2,16 +2,16 @@
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-#bash $SHELL_FOLDER/../common/kernel_6.6.sh
+bash $SHELL_FOLDER/../common/kernel_6.6.sh
 
-#git_clone_path master https://github.com/coolsnowwolf/lede target/linux/x86/files target/linux/x86/patches-6.10
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/x86/files target/linux/x86/patches-6.10
 
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/base-files/etc/board.d/02_network -P target/linux/x86/base-files/etc/board.d/
 
-#wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/64/config-6.6 -P target/linux/x86/64/
-#mv target/linux/x86/64/config-6.6 target/linux/x86/64/config-6.10
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/64/config-6.6 -P target/linux/x86/64/
+mv target/linux/x86/64/config-6.6 target/linux/x86/64/config-6.10
 
-#wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/firmware/linux-firmware/intel.mk -P package/firmware/linux-firmware/
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/firmware/linux-firmware/intel.mk -P package/firmware/linux-firmware/
 
 #wget -N https://raw.githubusercontent.com/mgz0227/openwrt/Dev/include/kernel-6.10 -P include/
 rm -rf target/linux/generic/backport-6.6/822-v6.11-0012-nvmem-Fix-return-type-of-devm_nvmem_device_get-in-ke.patch
