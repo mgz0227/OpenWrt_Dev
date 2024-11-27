372a1d3e8a374e52de03ae74a96bc6cf49ddbba3
miaogongzi <miaogongzi0227@gmail.com>
2024-11-27 18:59:00 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -20,7 +20,7 @@ wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kerne
 #rm -rf target/linux/generic/backport-6.6/409-mtd-spi-nor-winbond-fix-w25q128-regression.patch
 
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/usb.mk -P package/kernel/linux/modules/
-rm -rf target/linux/stm32/modules.mk
+rm -rf target/linux/stm32/
 
 rm -rf target/linux/generic/hack-6.12/531-debloat_lzma.patch
 rm -rf target/linux/generic/hack-6.12/410-block-fit-partition-parser.patch
