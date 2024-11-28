456531e0e575f0d640959b57213aa9bb18af8dc4
miaogongzi <miaogongzi0227@gmail.com>
2024-11-28 22:48:26 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -15,6 +15,7 @@ sed -i '/	refresh_config();/d' scripts/feeds
 
 rm -rf package/base-files
 mv -f feeds/miaogongzi/base-files package/
+mv -f feeds/packages/gst1-plugins-base
 
 echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
