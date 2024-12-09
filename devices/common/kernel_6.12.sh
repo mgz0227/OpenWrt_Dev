675250c67eb359044cb996f3851643519456f93e
miaogongzi <miaogongzi0227@gmail.com>
2024-12-10 00:50:50 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -3,9 +3,13 @@ shopt -s extglob
 
 wget -N https://raw.githubusercontent.com/mgz0227/Openwrt_6.12/main/include/kernel-6.12 -P include/
 
-
+wget -N https://raw.githubusercontent.com/openwrt/openwrt/main/target/linux/generic/backport-6.6/192-v6.12-fix-libbpf-Wmaybe-uninitialized.patch target/linux/x86/patches-6.12
 #git_clone_path 6.12 https://github.com/namiltd/openwrt include include
 git_clone_path master https://github.com/coolsnowwolf/lede package package
+
+rm -rf package/libs/mbedtls/*
+git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/libs/mbedtls package/libs/mbedtls
+
 rm -rf package/lean
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/hack-6.12 target/linux/generic/hack-6.12
 
