21c0b1a141f1783007c99d66c7b2b8da07427a31
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2024-12-10 02:37:18 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -10,6 +10,9 @@ git_clone_path master https://github.com/coolsnowwolf/lede package package
 rm -rf package/libs/mbedtls/*
 git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/libs/mbedtls package/libs/mbedtls
 
+rm -rf package/network/utils/linux-atm/*
+git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/utils/linux-atm package/network/utils/linux-atm
+
 rm -rf package/lean
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/hack-6.12 target/linux/generic/hack-6.12
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/backport-6.12 target/linux/generic/backport-6.12
