20d748e42742e15b022628841629ce6063a62193
miaogongzi <miaogongzi0227@gmail.com>
2024-12-11 00:51:39 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -30,6 +30,8 @@ git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/
 rm -rf package/kernel/rtw88-usb/*
 git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/kernel/rtw88-usb package/kernel/rtw88-usb
 
+rm -rf package/network/utils/iproute2/*
+git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/utils/iproute2 package/network/utils/iproute2
 rm -rf package/lean
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/hack-6.12 target/linux/generic/hack-6.12
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/backport-6.12 target/linux/generic/backport-6.12
