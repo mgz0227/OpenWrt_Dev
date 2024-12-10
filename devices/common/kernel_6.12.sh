ba6ee4a414403d74e6093971faf98103159715d1
miaogongzi <69211489+mgz0227@users.noreply.github.com>
2024-12-10 12:04:17 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -19,6 +19,8 @@ git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/
 rm -rf package/network/utils/tcpdump/*
 git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/utils/tcpdump package/network/utils/tcpdump
 
+rm -rf package/network/utils/iw/*
+git_clone_path openwrt-24.10 https://github.com/openwrt/openwrt package/network/utils/iw package/network/utils/iw
 rm -rf package/lean
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/hack-6.12 target/linux/generic/hack-6.12
 git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux/generic/backport-6.12 target/linux/generic/backport-6.12
