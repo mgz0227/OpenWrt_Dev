77de374238ac217727950f296cd7b1b142d6f52d
miaogongzi <miaogongzi0227@gmail.com>
2024-12-16 23:11:41 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -21,16 +21,5 @@ git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -P package/kernel/linux/modules/
 
 
-wget -N https://raw.githubusercontent.com/openwrt/openwrt/main/include/u-boot.mk -P include/
-
-cd feeds/packages
-rm -rf kernel libs/xr_usb_serial_common net/xtables-addons
-
-git_clone_path master https://github.com/openwrt/packages kernel libs/xr_usb_serial_common net/xtables-addons
-cd ../../
-
-sed -i "s/OpenWrt/MeowWrt/g" package/base-files/files/bin/config_generate package/base-files/image-config.in config/Config-images.in Config.in include/u-boot.mk include/version.mk package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh || true
-
-
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/hack-6.12
+#wget -N https://raw.githubusercontent.com/openwrt/openwrt/main/include/u-boot.mk -P include/
 
