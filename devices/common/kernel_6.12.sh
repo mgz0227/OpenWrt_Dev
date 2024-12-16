6325b8689b5436c85ddc062f01d5041cb3160ec5
miaogongzi <miaogongzi0227@gmail.com>
2024-12-17 00:35:49 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -5,35 +5,52 @@ shopt -s extglob
 #!/bin/bash
 
 # 删除旧文件夹（确保路径正确）
-rm -rf target/linux package/kernel package/boot package/firmware include scripts package/devel package/network config
+rm -rf target/linux package/kernel package/boot package/firmware include scripts  package/network config
 
-# 创建临时目录存放新代码
-TEMP_DIR="new"
-rm -rf "$TEMP_DIR"
-mkdir "$TEMP_DIR"
-cd "$TEMP_DIR"
+git_clone_path 6.12 https://github.com/namiltd/openwrt target/linux target/linux
+git_clone_path 6.12 https://github.com/namiltd/openwrt package/kernel package/kernel
+git_clone_path 6.12 https://github.com/namiltd/openwrt package/boot package/boot
+git_clone_path 6.12 https://github.com/namiltd/openwrt package/network/config/qosify package/network/config/qosify
+
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel-6.12 -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel-build.mk -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel-defaults.mk -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel-version.mk -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/kernel.mk -P include/
+
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/image-commands.mk -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/include/bpf.mk -P include/
+
+
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/trusted-firmware-a.mk -P include/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/include/trusted-firmware-a.mk -P include/
+
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/config/Config-images.in -P config/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/config/Config-kernel.in -P config/
+
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/scripts/ubinize-image.sh -P scripts/
+wget -N https://raw.githubusercontent.com/namiltd/openwrt/6.12/scripts/target-metadata.pl -P scripts/
+
+
+
+
+ package/network/config/qosify
+
 
 # 克隆指定分支代码
-git clone -b 6.12 --single-branch https://github.com/namiltd/openwrt.git || {
-    echo "Error: Failed to clone repository."
-    exit 1
-}
+#git clone -b 6.12 --single-branch https://github.com/namiltd/openwrt.git || {
+
 
 
 cd openwrt
 
 # 将需要的文件和目录复制到当前工作目录
-cp -rf --parents target/linux package/kernel package/boot package/firmware include/kernel* config/Config-images.in config/Config-kernel.in include/image*.mk include/trusted-firmware-a.mk include/bpf.mk scripts/ubinize-image.sh scripts/target-metadata.pl package/utils/bcm27xx-utils package/devel/perf package/network/config/qosify ../../
+#cp -rf --parents target/linux package/kernel package/boot package/firmware include/kernel* config/Config-images.in config/Config-kernel.in include/image*.mk include/trusted-firmware-a.mk include/bpf.mk scripts/ubinize-image.sh scripts/target-metadata.pl package/utils/bcm27xx-utils package/devel/perf package/network/config/qosify ../../
 
 cd ../../
 # 清理临时文件夹
-rm -rf "$TEMP_DIR"
+#rm -rf "$TEMP_DIR"
 
-# 检查关键文件是否存在
-if [ ! -f "target/linux/x86/image/64.mk" ] || [ ! -f "target/linux/x86/image/Makefile" ]; then
-    echo "Error: Required files are missing. Check the repository content."
-    exit 2
-fi
 
 sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk
 
