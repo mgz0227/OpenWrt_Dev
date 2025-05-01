fec73aa0630eb1415cdbba1a0fca9b75785b07dc
gongzi miao <miaogongzi0227@gmail.com>
2025-05-01 21:52:25 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -14,6 +14,9 @@ rm -rf package/devel/kselftests-bpf
 rm -rf package/devel/perf
 #rm -rf package/network/config/firewall4
 rm -rf packages/kernel/ovpn-dco
+rm -rf package/kernel/ovpn-dco
+rm -rf feeds/packages/kernel/ovpn-dco
+
 
 
 
