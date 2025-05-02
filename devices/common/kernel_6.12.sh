4787ac3e9b4c5c4ef62a44a96a3c41b535220b9d
miaogongzi <miaogongzi0227@gmail.com>
2025-05-02 12:04:42 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -10,6 +10,8 @@ git reset --hard origin/master
 
 cp -rf --parents target/linux package/kernel package/boot package/firmware include/kernel* config/Config-images.in config/Config-kernel.in include/image*.mk include/trusted-firmware-a.mk scripts/ubinize-image.sh package/utils/bcm27xx-utils package/network/config/qosify ../
 cd -
-
-find . -type d -name xtables-addons
-sudo find / -type d -name xtables-addons 2>/dev/null
+cd openwrt
+rm -rf ./feeds/packages/net/xtables-addons
+cd -
+#find . -type d -name xtables-addons
+#sudo find / -type d -name xtables-addons 2>/dev/null
