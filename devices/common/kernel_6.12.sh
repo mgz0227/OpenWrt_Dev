9c045242380e01263149a3369e27ae98f0366202
miaogongzi <miaogongzi0227@gmail.com>
2025-05-02 10:19:10 +0800

    Update kernel_6.12.sh

--- devices/common/kernel_6.12.sh
+++ devices/common/kernel_6.12.sh
@@ -10,3 +10,5 @@ git reset --hard origin/master
 
 cp -rf --parents target/linux package/kernel package/boot package/firmware include/kernel* config/Config-images.in config/Config-kernel.in include/image*.mk include/trusted-firmware-a.mk scripts/ubinize-image.sh package/utils/bcm27xx-utils package/network/config/qosify ../
 cd -
+
+find . -type d -name xtables-addons
