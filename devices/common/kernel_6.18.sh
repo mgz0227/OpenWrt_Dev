66e80f90c9d04066c91ef1d856f581bc23b2a6f2
gongzi miao <miaogongzi0227@gmail.com>
2025-12-21 02:05:24 +0800

    fix

--- devices/common/kernel_6.18.sh
+++ devices/common/kernel_6.18.sh
@@ -28,7 +28,8 @@ cd ../../
 
 
 cd package
-rm -rf devel/kselftests-bpf kernel/r8101 kernel/r8125 kernel/r8126 kernel/r8169
+rm -rf devel/kselftests-bpf 
+#kernel/r8101 kernel/r8125 kernel/r8126 kernel/r8169
 #devel/perf
 cd ../
 
@@ -42,6 +43,6 @@ sed -i -E '
 # 彻底关掉 mac80211 / cfg80211 / wifi 驱动，避免 backports 编译 ath9k
 sed -n '/# Wireless Drivers/,/# end of Wireless Drivers/p' .config | sed -e 's/=m/=n/' >> .config
 sed -i "s/\(kmod-qca.*\)=m/\1=n/" .config
-sed -i -E 's/^CONFIG_PACKAGE_kmod-(mac80211|cfg80211)=m$/# & is not set/' .config
-sed -i -E 's/^CONFIG_PACKAGE_kmod-(ath9k|ath9k-common|ath5k|iwlwifi|mt76.*)=m$/# & is not set/' .config
-sed -i -E 's/^CONFIG_PACKAGE_(MAC80211|CFG80211)_.+=y$/# & is not set/' .config
+#sed -i -E 's/^CONFIG_PACKAGE_kmod-(mac80211|cfg80211)=m$/# & is not set/' .config
+#sed -i -E 's/^CONFIG_PACKAGE_kmod-(ath9k|ath9k-common|ath5k|iwlwifi|mt76.*)=m$/# & is not set/' .config
+#sed -i -E 's/^CONFIG_PACKAGE_(MAC80211|CFG80211)_.+=y$/# & is not set/' .config
