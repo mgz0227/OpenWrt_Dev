9b8fd62ddf70e2894a65976e5e4b962fe370612d
miaogongzi <miaogongzi0227@gmail.com>
2025-12-22 10:01:57 +0800

    Update kernel_6.18.sh

--- devices/common/kernel_6.18.sh
+++ devices/common/kernel_6.18.sh
@@ -23,11 +23,11 @@ wget -N https://raw.githubusercontent.com/graysky2/packages/91f0b3e8621ae529d013
 wget -N https://raw.githubusercontent.com/graysky2/packages/91f0b3e8621ae529d01356bfe411c91d25e22b4b/libs/libpfring/patches/100-fix-compilation-warning.patch -P libs/libpfring/patches/
 wget -N libs/libpfring/patches/200-net-s-dev_get_flags-netif_get_flags.patch -P libs/libpfring/patches/
 wget -N https://raw.githubusercontent.com/graysky2/packages/91f0b3e8621ae529d01356bfe411c91d25e22b4b/libs/libpfring/Makefile -P libs/libpfring/
+cd ../../
 
 
 
-
-#cd feeds/packages
+cd feeds/packages
 rm -rf net/xtables-addons net/jool libs/libpfring
 git_clone_path master https://github.com/openwrt/packages net/jool
 wget -N https://raw.githubusercontent.com/graysky2/packages/c55afaa2bebca50a0e019a249c2748e7d7f745b7/kernel/ovpn-dco/Makefile -P kernel/ovpn-dco/
