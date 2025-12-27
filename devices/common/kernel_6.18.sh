d6577bcd4294e87aea4df07524228d5bbf777bd6
gongzi miao <miaogongzi0227@gmail.com>
2025-12-27 20:12:23 +0800

    Update kernel_6.18.sh

--- devices/common/kernel_6.18.sh
+++ devices/common/kernel_6.18.sh
@@ -30,10 +30,10 @@ cd ../../
 cd feeds/packages
 rm -rf net/xtables-addons net/jool 
 #libs/libpfring
-git_clone_path master https://github.com/openwrt/packages net/jool
+git_clone_path master https://github.com/openwrt/packages net/jool kernel/v4l2loopback/
 git_clone_path 6.18-xt-addons https://github.com/graysky2/packages net/xtables-addons
 wget -N https://raw.githubusercontent.com/graysky2/packages/c55afaa2bebca50a0e019a249c2748e7d7f745b7/kernel/ovpn-dco/Makefile -P kernel/ovpn-dco/
-wget -N https://raw.githubusercontent.com/graysky2/packages/668744be2cd07997a9d720d08bfaaac032bc009b/kernel/v4l2loopback/Makefile -P kernel/v4l2loopback/
+
 
 cd ../../
 
